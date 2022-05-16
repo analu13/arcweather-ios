bf7a2456133d6eaa9e09131c63ab8bd93af640f7
Analu Lisboa <analuizalisboa@gmail.com>
2022-05-16 17:52:19 -0300

    Adjusts OnBoarding constraints

--- arcweather-ios/Supporting Files/AppDelegate.swift
+++ arcweather-ios/Supporting Files/AppDelegate.swift
@@ -10,10 +10,18 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let storyboard = UIStoryboard(name: "OnBoarding", bundle: nil)
+        
+        let initialViewController = storyboard.instantiateViewController(withIdentifier: "OnBoardingViewController")
+        
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
