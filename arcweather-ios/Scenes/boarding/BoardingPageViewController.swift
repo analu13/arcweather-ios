380e1aece3bccc6517b7c6a24e176b11a25a35bf
Analu Lisboa <analuizalisboa@gmail.com>
2022-07-14 16:17:47 -0300

    updates project

--- arcweather-ios/Scenes/boarding/BoardingPageViewController.swift
+++ arcweather-ios/Scenes/boarding/BoardingPageViewController.swift
@@ -7,12 +7,81 @@
 
 import UIKit
 
+protocol BoardingPageViewControllerDelegate {
+    func didUpdatePageIndex(currentIndex: Int)
+}
+
 class BoardingPageViewController: UIPageViewController {
+
+    // MARK: - Properties
+
+    var pages = [UIViewController]()
+    var boardingDelegate: BoardingPageViewControllerDelegate?
+    var currentIndex = 0
+    var initialPage = 0
+
+    // MARK: - Actions
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        currentIndex.self = initialPage
+
+        dataSource = self
+     //   delegate = self
+
+        pages = [BoardingViewController.getInstance(index: 0),
+                 BoardingViewController.getInstance(index: 1),
+                 BoardingViewController.getInstance(index: 2)
+                 ]
+
+        setViewControllers([pages[initialPage]], direction: .forward, animated: true, completion: nil)
+
+    }
+
+    func forwardPage() {
+        currentIndex += 1
+        setViewControllers([pages[currentIndex]], direction: .forward, animated: true, completion: nil)
     }
 
 }
+
+extension BoardingPageViewController: UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+
+        let index = (pages.firstIndex(of: viewController)!)
+
+        if index > initialPage {
+            return pages[index - 1]
+        }
+
+        return nil
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+
+        let currentIndex = pages.firstIndex(of: viewController)!
+
+        if currentIndex < 2 {
+            return pages[currentIndex + 1]
+        }
+
+        return nil
+    }
+
+}
+
+// extension BoardingPageViewController: UIPageViewControllerDelegate {
+//    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+//        if completed {
+//            if let boardingViewController = pageViewController.viewControllers?.first as? BoardingViewController {
+//                currentIndex = boardingViewController.boardingIndex
+//                print("animating index: \(boardingViewController.boardingIndex)")
+//                print("current index: \(currentIndex)")
+//                boardingDelegate?.didUpdatePageIndex(currentIndex: currentIndex)
+//            }
+//        }
+//    }
+//
+// }
--- arcweather-ios/Scenes/boarding/BoardingViewController.swift
+++ arcweather-ios/Scenes/boarding/BoardingViewController.swift
@@ -9,20 +9,89 @@ import UIKit
 
 class BoardingViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var skipButton: UIButton!
+
+    @IBOutlet weak var mainImage: UIImageView!
+
+    @IBOutlet weak var pageControl: UIPageControl!
+
+    @IBOutlet weak var mainTitle: UILabel!
+
+    @IBOutlet weak var mainDescription: UILabel!
+
+    @IBOutlet weak var nextButton: UIButton!
+
+    // MARK: - Properties
+
+    var content = [ OnBoarding(title: "Welcome to ArcWeather",
+                               description: "Your new favorite weather app, with all the features you need to have the best information available.",
+                               image: "OnBoardingPage01" ),
+                    OnBoarding(title: "Follow different places",
+                               description: "You can easily add as many different locations as you wish.",
+                               image: "OnBoardingPage02" ),
+                    OnBoarding(title: "Never forget your umbrella",
+                               description: "Accurate weather forecast that you can trust to never get caught in the rain again.",
+                               image: "OnBoardingPage03" ) ]
+
+    var boardingPageViewController: BoardingPageViewController?
+
+    var boardingIndex = 0
+
+    // MARK: - Actions
+
+    @IBAction func skipButtonTapped(sender: UIButton) {
+        // TODO
+    }
+
+    @IBAction func nextButtonTapped(sender: UIButton) {
+
+        if boardingIndex >= 0 && boardingIndex < content.count - 1 {
+            boardingIndex += 1
+        }
+
+        didUpdatePageIndex(currentIndex: boardingIndex)
+        boardingPageViewController?.forwardPage()
+        updateUI()
+
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
-        // Do any additional setup after loading the view.
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension BoardingViewController {
+
+    static func getInstance (index: Int) -> BoardingViewController {
+        let viewController = UIStoryboard(name: "boarding", bundle: nil).instantiateViewController(withIdentifier: "BoardingViewController") as! BoardingViewController
+        viewController.boardingIndex = index
+        return viewController
     }
-    */
 
+    func updateUI () {
+
+        self.mainImage.image = UIImage(named: content[boardingIndex].image)
+        self.mainTitle.text = content[boardingIndex].title
+        self.mainDescription.text = content[boardingIndex].description
+
+        if boardingIndex == content.count - 1 {
+            self.skipButton.isHidden = true
+            self.nextButton.setTitle("Done", for: .normal)
+        }
+
+        pageControl.currentPage = boardingIndex
+
+    }
+
+}
+
+extension BoardingViewController: BoardingPageViewControllerDelegate {
+    func didUpdatePageIndex(currentIndex: Int) {
+        updateUI()
+    }
 }
